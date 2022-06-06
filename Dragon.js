048f27da2f7761a7a714264cae2ac05af70b7803
Pumpkintitan <pumpkintitan11@gmail.com>
2022-06-06 08:01:42 -0700

    eyes and 50 dragons

--- Dragon.js
+++ Dragon.js
@@ -1,18 +1,32 @@
 class Dragon {
-    constructor(x,y,r,g,b) {
+    constructor(x,y,r,g,b,h) {
         this.pos = createVector(x, y)
         this.vel = 0
-        this.acc = 0.01
-        this.head = -1
+        this.acc = 0
+        this.head = h
         this.color = [r,g,b]
+        this.scale = 0.5
+        this.dead = false
+        this.scales = [(Math.random()* 0.8) + 0.7,(Math.random()* 0.8) + 0.7,(Math.random()* 0.8) + 0.7]
+        this.rays = []
+        for (let i = 0; i < 9; i++) {
+            let rr = new Ray(-90 + (i * (45/2)))
+            this.rays.push(rr)
+        }
+        let rr = new Ray(180)
+        this.rays.push(rr)
+        this.frame = 0
     }
     
-    draw(sh, sw, st, debug) {
+    draw(debug, walls) {
+        this.frame += 0.1
+        let tscalew = this.scales[0] += Math.sin(this.frame) * 0.01
         push()
         rectMode(CORNERS);
         noStroke()
         translate(this.pos.x, this.pos.y)
         rotate(-this.head)
+        scale(this.scale)
         if (debug) {
             // body
             fill('black')
@@ -20,25 +34,25 @@ class Dragon {
 
             // right wing
             fill('red')
-            quad(-10, -5, -10, 5, -40*sw, 25*((sw+0.5)/1.5), -30*sw, 0*sw)
+            quad(-10, -5, -10, 5, -40*tscalew, 25*((tscalew+0.5)/1.5), -30*tscalew, 0*tscalew)
             fill('orange')
-            quad(-40*sw, 25*((sw+0.5)/1.5), -55*sw, 20*((sw+0.5)/1.5), -70*sw, -15, -30*sw, 0*sw)
+            quad(-40*tscalew, 25*((tscalew+0.5)/1.5), -55*tscalew, 20*((tscalew+0.5)/1.5), -70*tscalew, -15, -30*tscalew, 0*tscalew)
 
             // left wing
             fill('blue')
-            quad(10, -5, 10, 5, 40*sw, 25*((sw+0.5)/1.5), 30*sw, 0*sw)
+            quad(10, -5, 10, 5, 40*tscalew, 25*((tscalew+0.5)/1.5), 30*tscalew, 0*tscalew)
             fill('purple')
-            quad(40*sw, 25*((sw+0.5)/1.5), 55*sw, 20*((sw+0.5)/1.5), 70*sw, -15, 30*sw, 0*sw)
+            quad(40*tscalew, 25*((tscalew+0.5)/1.5), 55*tscalew, 20*((tscalew+0.5)/1.5), 70*tscalew, -15, 30*tscalew, 0*tscalew)
 
             // head
             fill('yellow')
-            quad(-10, 20, 10, 20, 13*((sh+1)/2), 30*sh, -13*((sh+1)/2), 30*sh)
+            quad(-10, 20, 10, 20, 13*((this.scales[1]+1)/2), 30*this.scales[1], -13*((this.scales[1]+1)/2), 30*this.scales[1])
             fill('darkgoldenrod')
-            quad(-7*((sh+1)/2), 50*sh, 7*((sh+1)/2), 50*sh, 13*((sh+1)/2), 30*sh, -13*((sh+1)/2), 30*sh)
+            quad(-7*((this.scales[1]+1)/2), 50*this.scales[1], 7*((this.scales[1]+1)/2), 50*this.scales[1], 13*((this.scales[1]+1)/2), 30*this.scales[1], -13*((this.scales[1]+1)/2), 30*this.scales[1])
 
             // tail
             fill('green')
-            quad(-7*st, -25, 7*st, -25, 2, -80*st, -2, -80*st)
+            quad(-7*this.scales[2], -25, 7*this.scales[2], -25, 2, -80*this.scales[2], -2, -80*this.scales[2])
 
             // hit box
             noFill()
@@ -53,26 +67,56 @@ class Dragon {
             }
             fill(cc[0], cc[1], cc[2])
             rect(-10,-25, 10, 20)
-            quad(-10, -5, -10, 5, -40*sw, 25*((sw+0.5)/1.5), -30*sw, 0*sw)
-            quad(10, -5, 10, 5, 40*sw, 25*((sw+0.5)/1.5), 30*sw, 0*sw)
-            quad(-10, 20, 10, 20, 13*((sh+1)/2), 30*sh, -13*((sh+1)/2), 30*sh)
+            quad(-10, -5, -10, 5, -40*tscalew, 25*((tscalew+0.5)/1.5), -30*tscalew, 0*tscalew)
+            quad(10, -5, 10, 5, 40*tscalew, 25*((tscalew+0.5)/1.5), 30*tscalew, 0*tscalew)
+            quad(-10, 20, 10, 20, 13*((this.scales[1]+1)/2), 30*this.scales[1], -13*((this.scales[1]+1)/2), 30*this.scales[1])
             
             
             fill(this.color[0],this.color[1],this.color[2])
-            quad(-40*sw, 25*((sw+0.5)/1.5), -55*sw, 20*((sw+0.5)/1.5), -70*sw, -15, -30*sw, 0*sw)
-            quad(40*sw, 25*((sw+0.5)/1.5), 55*sw, 20*((sw+0.5)/1.5), 70*sw, -15, 30*sw, 0*sw)
-            quad(-7*((sh+1)/2), 50*sh, 7*((sh+1)/2), 50*sh, 13*((sh+1)/2), 30*sh, -13*((sh+1)/2), 30*sh)
-            quad(-7*st, -25, 7*st, -25, 2, -80*st, -2, -80*st)
+            quad(-40*tscalew, 25*((tscalew+0.5)/1.5), -55*tscalew, 20*((tscalew+0.5)/1.5), -70*tscalew, -15, -30*tscalew, 0*tscalew)
+            quad(40*tscalew, 25*((tscalew+0.5)/1.5), 55*tscalew, 20*((tscalew+0.5)/1.5), 70*tscalew, -15, 30*this.scales[0], 0*this.scales[0])
+            quad(-7*((this.scales[1]+1)/2), 50*this.scales[1], 7*((this.scales[1]+1)/2), 50*this.scales[1], 13*((this.scales[1]+1)/2), 30*this.scales[1], -13*((this.scales[1]+1)/2), 30*this.scales[1])
+            quad(-7*this.scales[2], -25, 7*this.scales[2], -25, 2, -80*this.scales[2], -2, -80*this.scales[2])
 
         }
         pop()
+        if (debug) {
+            this.showRays(walls)
+        }
+    }
+
+    showRays(walls) {
+        for(let i = 0; i < this.rays.length; i++) {
+            this.rays[i].draw(this.head, this.pos, walls)
+        }
+    }
+
+    action() {
+        let c = Math.floor(Math.random()*4)
+        switch(c) {
+            case 0:
+                this.acc += 0.01
+                break;
+            case 1:
+                this.acc -= 0.01
+                break;
+            case 2:
+                this.head += 0.03
+                break;
+            case 3:
+                this.head -= 0.03
+                break;
+        }
     }
 
     move() {
-        this.head += 0.015
         this.vel += this.acc
+        this.vel *= 0.95
         if (this.vel > 5) {
             this.vel = 5
+        }
+        if (this.vel < -1) {
+            this.vel = -1
         }
         let sa = Math.sin(this.head)
         let ca = Math.cos(this.head)
@@ -80,20 +124,9 @@ class Dragon {
         this.pos.y += ca * this.vel
     }
 
-    undomove() {
-        let sa = Math.sin(this.head)
-        let ca = Math.cos(this.head)
-        this.pos.x -= sa * this.vel
-        this.pos.y -= ca * this.vel
-        this.vel -= this.acc
-        this.head -= 0.01
-    }
-
     checkIntercept(wall) {
-        this.move()
-        if (collideLineCircle(wall.l1.x, wall.l1.y, wall.l2.x, wall.l2.y, this.pos.x, this.pos.y, 60)){
-            this.undomove()
-            this.head += Math.PI/2
+        if (collideLineCircle(wall.l1.x, wall.l1.y, wall.l2.x, wall.l2.y, this.pos.x, this.pos.y, (this.scale * 60))){
+            this.dead = true
         }
     }
 }
---
+++ Ray.js
@@ -0,0 +1,29 @@
+class Ray {
+    constructor(ang) {
+        this.a = ang
+        this.len = 1500
+    }
+
+    draw(dhead, dpos, walls) {
+        this.len = this.compute(dhead, dpos, walls)
+        let b = radians(this.a)
+        let sa = Math.sin(dhead + b)
+        let ca = Math.cos(dhead + b)
+        stroke('red')
+        line(dpos.x, dpos.y, dpos.x + (sa * this.len), dpos.y + (ca * this.len))
+    }
+
+    compute(dhead, dpos, walls) {
+        let tlen = 1500
+        let b = radians(this.a)
+        let sa = Math.sin(dhead + b)
+        let ca = Math.cos(dhead + b)
+        for(let i = 0; i < walls.length; i++) {
+            let hit = collideLineLine(dpos.x, dpos.y, dpos.x + (sa * tlen), dpos.y + (ca * tlen), walls[i].l1.x, walls[i].l1.y, walls[i].l2.x, walls[i].l2.y, true)
+            if (hit.x) {
+                tlen = dist(dpos.x, dpos.y, hit.x, hit.y)
+            }
+        }
+        return tlen
+    }
+}
